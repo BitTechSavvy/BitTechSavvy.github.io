eb57ab8032296defe73fc32fcbe8d34cbfc3452e
Avneet <141897546+BitTechSavvy@users.noreply.github.com>
2023-08-28 18:09:15 +0530

    Add files via upload

---
+++ send_email.php
@@ -0,0 +1,16 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+if (isset($_POST['submit'])) {
+    $to = "av26.singh@gmail.com"; 
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+    $headers = "From: " . $_POST['email'];
+
+    if (mail($to, $subject, $message, $headers)) {
+        echo "Email sent successfully!";
+    } else {
+        echo "Error sending email.";
+    }
+}
